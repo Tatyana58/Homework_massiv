5cba0855384d1efd21ec6cdb254868535886a503
Tatyana58 <tatbir@rambler.ru>
2022-11-28 20:20:46 +0300

    Beginning Array-Option 1 Array-Option 2 Task 1

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,27 @@ import static com.sun.tools.doclint.Entity.or;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашняя работа - Массивы");
-        int[] weights={90,91,93,85,87,88,89,0,0,0,0};
-        weights[0]=90;
-        int januaryWeight=weights[0];
-        System.out.println(januaryWeight);
-        System.out.println(weights[0]);
-        System.out.println(weights[4]);
-        int january=0;
-        System.out.println(weights[january]);
-        for (int i = 0; i < 11; i++) {
-            System.out.println(weights[i]);
+
+        System.out.println("Задание №1");
+        // 1 задача
+        //Объявите три массива:
+        //1. Целочисленный массив, заполненный тремя цифрами – 1, 2 и 3 – с помощью ключевого слова new.
+        int[] id = new int[3];
+        int k=1;
+        for (int i=0; i<3;i++){
+            id[i]=k;
+            k++;
+            System.out.println("Заполнили массив id ["+i+"] = "+id[i]);
         }
+        System.out.println(" ");
+
+
+        System.out.println("Задание №2");
+        //2. Массив, в котором можно хранить три дробных числа – 1.57, 7.654, 9.986 –  сразу заполнив его значениями.
+
+
+
+        //3. Произвольный массив – тип и количество данных определите сами.
+        // Самостоятельно выберите способ создания массива: с помощью ключевого слова или сразу заполненный элементами.
     }
 }
