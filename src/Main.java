255afebb65e3a8faddc00ca750a5b97489aa5912
Tatyana58 <tatbir@rambler.ru>
2022-11-28 19:20:47 +0300

    Beginning Array-Option 1 Array-Option 2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import static com.sun.tools.doclint.Entity.or;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашняя работа - Массивы");
-        int[] weights=new int [12];
+        int[] weights={90,91,93,85,87,88,89,0,0,0,0};
         weights[0]=90;
         int januaryWeight=weights[0];
         System.out.println(januaryWeight);
@@ -11,7 +11,7 @@ public class Main {
         System.out.println(weights[4]);
         int january=0;
         System.out.println(weights[january]);
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < 11; i++) {
             System.out.println(weights[i]);
         }
     }
