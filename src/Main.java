7a441875b69ace3fc3909a5a556cd5bd04a5558a
Tatyana58 <tatbir@rambler.ru>
2022-11-28 22:40:00 +0300

    Beginning Array-Option 1 Array-Option 2 Task 1 Task 2 Task 3 Task 4 Task 5 Task 6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import static com.sun.tools.doclint.Entity.forall;
-import static com.sun.tools.doclint.Entity.or;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашняя работа - Массивы");
@@ -55,20 +52,87 @@ public class Main {
         //Пройдите по каждому из трех массивов и распечатайте все элементы всех трех массивов, начиная с первого элемента,
         //через запятую. Запятая между последним элементом одного массива и первым элементом следующего не нужна.
         //см выше.
-        int x=3;
-        System.out.print(id[0]+","+id[1]+","+id[2]+"  ");
-        //System.out.print(chickenWeight[y] + "," + chickenWeight[y += 1] + "," + chickenWeight[y += 1] + " ");
-        int y=0;
-        while (y<3) {
-            System.out.print(chickenWeight[y]);
-           if(y<2){
+        int h=3; // длинна массива
+        for (int x=0; x<h;x++) {
+            System.out.print(id[x]);
+            if (x<h-1){
                 System.out.print(",");
-            }else {
-               System.out.print(" ");
-           }
-            y++;
+            }
         }
-            System.out.print(datyBirth[0] + "," + datyBirth[1] + "," + datyBirth[2] + " ");
-            System.out.print(productLong + "," + productWidth + "," + productHeight + "," + productWeight);
+        System.out.println(" ");
+        for (int x=0; x<h;x++) {
+            System.out.print(chickenWeight[x]);
+           if(x<h-1){
+                System.out.print(",");
+            }
+        }
+        System.out.println(" ");
+
+        for (int x=0;x<h;x++) {
+            System.out.print(datyBirth[x]);
+            if(x<h-1){
+                System.out.print(",");
+            }
+        }
+        System.out.println(" ");
+        System.out.println(productLong + "," + productWidth + "," + productHeight + "," + productWeight);
+        System.out.println(" ");
+
+
+        System.out.println("3 задача.");
+        //3 задача
+        //Теперь ваша задача – распечатать все элементы всех трех массивов, но начинать нужно не с первого элемента массива,
+        // а с последнего. Элементы должны быть распечатаны через запятую, при этом элементы одного массива располагаются на одной строчке,
+        // а элементы другого массива – на другой.
+        //Запятая между последним элементом одного массива и первым элементом следующего не нужна.
+        //Если во втором задании в консоль у вас вывелся результат
+        //1, 2, 3
+        //1.57, 7.654, 9.986
+        //*произвольные элементы третьего массива*
+        //То в третьем задании результат должен быть
+        // 3, 2, 1
+        //9.986, 7.654, 1.57
+        //*произвольные элементы третьего массива в обратном порядке*
+        int l=0; // длинна массива
+        for (int x=2;x>=l;x--) {
+            System.out.print(id[x]);
+            if (x>=l+1){
+                System.out.print(",");
+            }
+        }
+        System.out.println(" ");
+        for (int x=2;x>=l;x--) {
+            System.out.print(chickenWeight[x]);
+            if(x>=l+1){
+                System.out.print(",");
+            }
+        }
+        System.out.println(" ");
+
+        for (int x=2;x>=l;x--) {
+            System.out.print(datyBirth[x]);
+            if(x>=l+1){
+                System.out.print(",");
+            }
+        }
+        System.out.println(" ");
+        System.out.println("Вариант 1");
+        System.out.println ( productWeight+ "," + productHeight + "," +  productWidth+ "," + productLong);
+        System.out.println("Вариант 2");
+        for (int x=3;x>=l;x--) {
+            System.out.print(product[x]);
+            if(x>=l+1){
+                System.out.print(",");
+            }
+        }
+        System.out.println(" ");
+
+        System.out.println("4 задача.");
+        // 4 задача
+        //Пройдитесь по первому целочисленному массиву и все нечетные числа в нем сделайте четными (нужно прибавить 1).
+        //Важно: код должен работать с любым целочисленным массивом, поэтому для решения задания вам нужно использовать циклы.
+        //Распечатайте результат преобразования в консоль.
+
+
         }
 }
