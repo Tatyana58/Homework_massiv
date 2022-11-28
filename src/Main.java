8dabd697254122ad2fbdeffbdcb16c4147fc3ab5
Tatyana58 <tatbir@rambler.ru>
2022-11-28 19:12:19 +0300

    Beginning Array-Option 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Домашняя работа - Массивы");
+        int[] weights=new int [12];
+        weights[0]=90;
+        int januaryWeight=weights[0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+        int january=0;
+        System.out.println(weights[january]);
+
+
     }
 }
